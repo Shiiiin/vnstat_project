0e361d5c5c4ba956f188f35c414af0a29c0f07f6
Hyunsik Park <hsdogfoot@gmail.com>
2019-12-07 18:25:36 +0900

    zombie_num

---
+++ src/zombie_num.h
@@ -0,0 +1,70 @@
+#include <dirent.h>
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+
+#define DECIMAL (10)
+#define BUFFER_SIZE (1024)
+
+void zombie_num(void)
+{
+        DIR* directory;
+        struct dirent* entry;
+        char* endptr;
+        long pid;
+
+        char buffer[BUFFER_SIZE];
+        FILE* file;
+
+        char* process_pid;
+        char* process_name;
+        char* process_state;
+        char* process_ppid;
+
+        size_t zombie_count = 0;
+
+        directory = opendir("/proc");
+        if (directory == NULL) {
+                printf("directory open failed!\n");
+                return -1;
+        }
+
+        while ((entry = readdir(directory)) != NULL) {
+                pid = strtol(entry->d_name, &endptr, DECIMAL);
+                if (*endptr != '\0') {
+                        continue;
+                }
+
+                sprintf(buffer, "/proc/%ld/stat", pid);
+                file = fopen(buffer, "r");
+                if (file == NULL) {
+                        printf("stat file open failed!\n");
+                        continue;
+                }
+
+                if (fgets(buffer, sizeof(buffer), file) == NULL) {
+                        printf("stat file read failed!\n");
+                        fclose(file);
+                        continue;
+                }
+
+                process_pid = strtok(buffer, " ");
+                process_name = strtok(NULL, " ");
+                process_state = strtok(NULL, " ");
+                process_ppid = strtok(NULL, " ");
+
+                if (!strcmp(process_state, "Z")) {
+                        ++zombie_count;
+                }
+
+                fclose(file);
+        }
+
+        closedir(directory);
+
+        printf("%d zombies detected... !!\n", zombie_count);
+
+        return 0;
+}
+
